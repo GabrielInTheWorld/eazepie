316d238b1dea32d74697e08d5bceba5cc0b9f7ad
Gabriel Meyer <meyergabriel@live.de>
2020-03-31 21:25:32 +0200

    initial commit

---
+++ EazePie.cpp
@@ -0,0 +1,33 @@
+﻿// EazePie.cpp : Defines the entry point for the application.
+//
+
+#include "EazePie.h"
+
+using namespace std;
+
+int main() {
+    string line;
+    stringstream ss;
+    cout << "Hello CMake." << endl;
+
+    ifstream file("hello.txt");
+    file >> line;
+    cout << "Line <<" << line << endl;
+    getline(file, line);
+
+    //ifstream file;
+    //file.open("./hello.txt");
+
+    /*if ( file.is_open() ) {
+        while ( getline(file, line) ) {
+            cout << line << endl;
+        }
+        file.close();
+    } else {
+        cout << "Unable to open file." << endl;
+    }*/
+    getline(file, line);
+    cout << "Line: " << line << endl;
+    file.close();
+    return 0;
+}
---
+++ EazePie.h
@@ -0,0 +1,14 @@
+﻿// EazePie.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+#include <fstream>
+#include <ostream>
+#include <string>
+#include <sstream>
+
+using namespace std;
+
+// TODO: Reference additional headers your program requires here.
